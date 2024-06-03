e58eb1813e1fdf9ab59d1930646f4236d40e623f
ff0xxx <poisk8302@email.com>
2024-06-04 00:40:48 +0300

    add: 3rd parser refactor: 1st 2nd parsers

--- sites/lingua-academ.py
+++ sites/lingua-academ.py
@@ -1,16 +1,16 @@
 from bs4 import BeautifulSoup
 from requests import get
-from pprint import pprint
 
 URL = 'https://lingua-academ.ru/blog/380_populyarnih_razgovornih_fraz_na_angliiskom'
+
 
 def lingua_academ():
     response = get(URL)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'lxml')
 
-    tables_list = soup.find('div', class_='article_body').find_all('tbody')
     themes_list: list[str] = [theme.text.strip() for theme in soup.select('[id^="anchor_"]')]
+    tables_list = soup.find('div', class_='article_body').find_all('tbody')
     phrases: dict[str: dict] = dict()
 
     for theme, table in zip(themes_list, tables_list):
@@ -18,11 +18,10 @@ def lingua_academ():
 
         for tr in table.find_all('tr'):
             p_list = tr.find_all('p')
-            theme_phrases[p_list[0].text.strip()] = p_list[1].text.strip()
+            eng = p_list[0].text.strip()
+            rus = p_list[1].text.strip()
+            theme_phrases[eng] = rus
 
         phrases[theme] = theme_phrases
 
     return phrases
-
-
-pprint(lingua_academ())
--- sites/smileenglish.py
+++ sites/smileenglish.py
@@ -1,16 +1,16 @@
 from bs4 import BeautifulSoup
 from requests import get
-from pprint import pprint
 
 URL = 'https://smileenglish.ru/technique/anglijskij-dlya-turistov-200-fraz/'
+
 
 def smileenglish():
     response = get(URL)
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'lxml')
 
-    tables_list = soup.find_all('tbody')
     themes_list: list[str] = [theme.text.strip() for theme in soup.select('h2 > strong')[1:] if theme.text.strip()]
+    tables_list = soup.find_all('tbody')
     phrases: dict[str: dict] = dict()
 
     for theme, table in zip(themes_list, tables_list):
@@ -18,10 +18,10 @@ def smileenglish():
 
         for tr in table.find_all('tr')[1:]:
             p_list = tr.find_all('p')
-            theme_phrases[p_list[1].text.strip().replace('\xa0', ' ')] = p_list[0].text.strip().replace('\xa0', ' ')
+            eng = p_list[1].text.strip()
+            rus = p_list[0].text.strip()
+            theme_phrases[eng] = rus
 
         phrases[theme] = theme_phrases
 
     return phrases
-
-pprint(smileenglish())
